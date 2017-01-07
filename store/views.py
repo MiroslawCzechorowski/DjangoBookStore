1aab1945709420db084f5ed58d6ef58413537e9c
Miroslaw Czechorowski <miroslawczechorowski@gmail.com>
2017-01-07 20:26:42 +0100

    Done to task 5

--- store/views.py
+++ store/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Book
 # Create your views here.
+
+
+def books_list(request):
+    books = Book.objects.all()
+    return render(request, 'books.html', {'books': books})
